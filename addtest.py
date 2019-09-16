99f1d32b50885bdb14fcadf45c8cd6c9827bc4e9
Lily Ha <lily.ha909@gmail.com>
2019-09-16 18:27:40 -0400

    Update addtest.py
    Here is the test. I also allow the user to input the values that they want to add.

--- addtest.py
+++ addtest.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python3 # -*- coding: utf-8 -*-
+""" Created on 09/16/2019
+@author: Lily Ha
+@collaborators:
+"""
 """
   Edit the following so that it will take the two inputs and return their sum.
   You should only need to add 1 or 2 lines to accomplish this :)
 """
 def add(a, b):
+    return(a+b)
     
+if __name__ == '__main__':
+    a = print(input('Enter first value:'))
+    b = print(input('Enter next value:'))
+    added = add(a,b)
+    print('The sum is:',added)
